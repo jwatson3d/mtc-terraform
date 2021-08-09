edb154d1d1f692009309ada184b9dd3fd2ec8596
John Watson <jwatson3d@gmail.com>
2021-08-09 20:55:36 +0000

    Generate random container name and external port

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -11,21 +11,27 @@ terraform {
 
 provider "docker" {}
 
+resource "random_string" "random" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
 resource "docker_container" "nodered_container" {
-  name  = "nodered"
+  name  = join("-", ["nodered", random_string.random.result])
   image = docker_image.nodered_image.latest
   ports {
     internal = 1880
-    external = 1880
+    #external = 1880 # docker will chose a random one
   }
 }
 
 output "ip-address" {
-  value       = join(":",[docker_container.nodered_container.ip_address,docker_container.nodered_container.ports[0].external])
+  value       = join(":", [docker_container.nodered_container.ip_address, docker_container.nodered_container.ports[0].external])
   description = "IP address and external port of the container"
 }
 
